2411e0f8a061b1ebfa25a6651e27e656660dcc77
kishtel <ktn12375@gmail.com>
2021-02-23 11:03:25 +0200

    Page Object Main_page Login_page

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # stepic_page_object
 stepic_page_object
 Stepic Task Page Object
-Page Object directiry pages class init ver 1.1.1
+Page Object directiry page class init ver 1.1.1
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,7 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 class MainPage(BasePage): 
     def go_to_login_page(self):
@@ -8,3 +9,7 @@ class MainPage(BasePage):
         login_link.click()
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+    def go_to_login_page(self):
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+         
