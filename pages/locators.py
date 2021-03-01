2d0f42955be6b92de8b355c1ccf5b358fb105fcd
kishtel <ktn12375@gmail.com>
2021-03-01 10:17:29 +0200

    Page Object Product_page add

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,12 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM=(By.ID,"login_form")
     LOOGIN_REGISTER_FORM=(By.ID,"register_form")
+class ProductPageLocators():
+    BUTTON_SUBMIT= (By.CSS_SELECTOR,".btn.btn-lg.btn-primary.btn-add-to-basket")
+    BUTTON_REVIEW=(By.ID,"write_review")
+    BUTTON_WISH=(By.CSS_SELECTOR,"btn.btn-lg.btn-wishlist")
+    PRODUCT_FORM=(By.ID,"add_to_basket_form")
+    PRODUCT_PRICE_BACKET=(By.XPATH,"//div[@class='alertinner ']/p[1]/strong")
+    PRODUCT_PRICE_MAIN=(By.CSS_SELECTOR,".product_main .price_color")
+    PRODUCT_NAME=(By.CSS_SELECTOR,".product_main h1")
+    PRODUCT_NAME_BACKET=(By.XPATH,"//div[@id='messages']/div[contains(@class,'alert-success')][1]/div/strong")
